044fe4a8e42e09093b90a5fc16a6a32919d2ca4d
DarshanLevi <s2darshanrider@gmail.com>
2024-08-01 12:22:40 +0530

    update: 3 CRUD with mongoose

--- index.js
+++ index.js
@@ -1,61 +1,79 @@
 const express = require('express');
-const app = express();
+const Item = require('./models/item');
+const dotenv = require('dotenv')
+const mongoose = require('mongoose')
+
+
+dotenv.config()
+const app = express()
 const port = 3000;
 
 app.use(express.json());
 
-// In-memory data storage
-let items = [
-    {
-      id: 1,
-      name: "oneplus nord 1"
-    },
-    {
-      id: 2,
-      name: "samsung galaxy s24"
+mongoose.connect(process.env.MONGODB_URI)
+.then(() => console.log('connected to MongoDB'))
+.catch(err => console.error('could not connect to MongoDB', err))
+
+app.get('/items', async (req,res) => {
+    try{
+        const items = await Item.find()
+        res.json(items)
+    }catch (err){
+        res.status(500).send('Internal server error')
+
     }
-  ];
+});
 
-app.get('/items', (req,res) => {
-    res.json(items)
-})
-
-app.get('/items/:id', (req,res) =>{
-    const item = items.find(i => i.id === parseInt(req.params.id))
+app.get('/items/:id', async (req,res) =>{
+    try {
+    const item = await Item.findById((req.params.id))
     if (!item) return res.status(404).send('item not found')
         res.json(item)
+      }catch(err){
+        res.status(500).send('Internal Server Error')
+      }
+});
+
+app.post('/items', async (req, res) => {
+    const newItem = new Item({
+      name: req.body.name
+    });
+    try{
+        const savedItem = await newItem.save()
+        res.status(201).json(savedItem)
+    }catch(err){
+        res.status(500).send('Internal Server Error')
+    }
+});
+
+app.put('item/:id', async (req,res) =>{
+    try {
+    const updatedItem = await Item.findByIdAndUpdate(req.params.id,
+        {name: req.body.name},
+        {new: true}
+    )
+    if (!updatedItem) return res.status(404).send("item not found")
+        res.json(updatedItem);
+}catch(err){
+    res.status(500).send('Internal Server Error')
+    }
+});
+
+app.delete('/item/:id', async (req,res) => {
+    try{
+    const deleteItem = await Item.findByIdAndDelete(req.params.id)
+    if (!deleteItem)return res.status(404).send('item not found')
+        res.json(deleteItem)
+    }catch(err){
+        res.status(500).send('Internal Server Error')
+    }
 })
 
-app.post('/items', (req,res)=>{
-    const newItem = {
-        id: items.length + 1,
-        name: req.body.name
-    };
-    items.push(newItem)
-    res.status(201).json(newItem)
-})
-
-app.put('item/:id', (req,res) =>{
-    const item = items.find(i => i.id === parseInt(req.params.id))
-    if (!item) return res.status(404).send("item not found")
-
-        item.name = req.body.name;
-        res.json(item)
-})
-
-app.delete('/item/:id', (req,res) => {
-    const itemIndex = items.findIndex(i => i.id === parseInt(req.params.id))
-    if (itemIndex === -1) return res.status(404).send('item not found')
-
-        const deletedItem = items.splice(itemIndex, 1)
-        res.json(deletedItem)
-})
 
 
-
-app.get('/', (req,res) =>{
-    res.send('hello world')
-})
+// app.get('/', (req,res) =>{
+//     res.send('hello world')
+// })
 
 app.listen(port, () => {
     console.log(`server is running on http://localhost:${port}`)
